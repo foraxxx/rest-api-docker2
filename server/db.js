8c0532a6891221f53fac90d1fddecc26601f90fb
foraxru <foraxru@gmail.com>
2024-11-27 18:33:23 +0300

    change compose

--- server/db.js
+++ server/db.js
@@ -1,16 +1,16 @@
 import {Sequelize} from 'sequelize'
 
-// process.loadEnvFile(".env")
+process.loadEnvFile(".env")
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,
-  // process.env.DB_NAME,
-  // process.env.DB_USER,
-  // process.env.DB_PASSWORD,
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     dialect: 'postgres',
     protocol: 'postgres',
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
   }
 )
 
